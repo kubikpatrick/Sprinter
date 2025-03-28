a17646d862befb626538c629f6a3562ae4a21f05
belmo <belmokhtarpatrick@gmail.com>
2025-03-28 12:55:24 +0100

    feat: add default account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using Sprinter.Models.Identity;
+
+namespace Sprinter.Controllers;
+
+[Authorize]
+[Route("account")]
+public sealed class AccountController : Controller
+{
+    private readonly UserManager<User> _userManager;
+
+    public AccountController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+}
