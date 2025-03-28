814a47784f2646aad5ff338bcebf24b4bd985a29
belmo <belmokhtarpatrick@gmail.com>
2025-03-28 11:44:00 +0100

    feat: add all role names

---
+++ Models/Identity/RoleName.cs
@@ -0,0 +1,8 @@
+namespace Sprinter.Models.Identity;
+
+public readonly struct RoleName
+{
+    public const string Admin = "Admin";
+    public const string Passenger = "Passenger";
+    public const string Driver = "Driver";
+}
