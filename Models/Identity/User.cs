9086f87672c4c8690feeb29651f42ff260c48516
belmo <belmokhtarpatrick@gmail.com>
2025-03-28 11:07:32 +0100

    feat: add `User` model with its own properties

---
+++ Models/Identity/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Sprinter.Models.Identity;
+
+public sealed class User : IdentityUser
+{
+    public User()
+    {
+
+    }
+
+    [StringLength(32)]
+    [Required]
+    [PersonalData]
+    public string FirstName { get; internal set; }
+
+    [StringLength(32)]
+    [Required]
+    [PersonalData]
+    public string LastName { get; internal set; }
+
+    [Required]
+    public DateTime CreatedAt { get; internal set; }
+
+    [NotMapped]
+    public string FullName => $"{FirstName} {LastName}";
+}
