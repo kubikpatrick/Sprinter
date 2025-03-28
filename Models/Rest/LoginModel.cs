a4c246961fcf5f6b03ba711a925669ffc9dc6c5d
belmo <belmokhtarpatrick@gmail.com>
2025-03-28 12:49:46 +0100

    feat: add REST models for authentication

---
+++ Models/Rest/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sprinter.Models.Rest;
+
+public sealed class LoginModel
+{
+    public LoginModel()
+    {
+
+    }
+
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Email is required.")]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
+
+    [Display(Name = "Remember me")]
+    public bool RememberMe { get; set; } = false;
+}
---
+++ Models/Rest/SignUpModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sprinter.Models.Rest;
+
+public sealed class SignUpModel
+{
+    public SignUpModel()
+    {
+        
+    }
+
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Email is required.")]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "First name is required.")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required.")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Avatar is required.")]
+    public IFormFile Avatar { get; set; }
+}
